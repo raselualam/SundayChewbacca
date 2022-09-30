3e75f03cd41da3653c41b6e3a4e262450b7372ff
silvi <silvisnigdha09@gmail.com>
2022-09-29 23:22:23 -0400

    review class

--- src/com/upskill/assignment_1/Assignment_1_1.java
+++ src/com/upskill/assignment_1/Assignment_1_1.java
@@ -11,8 +11,8 @@ public class Assignment_1_1 {
 		}
 		
 		public static void trianglePerimeter() {
-			int a = 5;
-			int b = 7;
+			int a = 50;
+			int b = 70;
 			int c = 11;
 			int perimeter = a+b+c;
 			System.out.println("The Perimeter of Triangle with arms 5, 7, 11 is " + perimeter);
